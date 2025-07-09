9d904f928c3ab2a34288028ac76a62c802ae7f82
Daniel <daniel.albert.liu@gmail.com>
2025-07-08 21:49:12 -0400

    day 1 part 2

--- 2024/rust/day-01/src/main.rs
+++ 2024/rust/day-01/src/main.rs
@@ -1,9 +1,19 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 
-fn main() -> io::Result<()> {
-    let file = File::open("./input.txt")?;
-    let lines = io::BufReader::new(file).lines();
+fn main() {
+    if let Ok(dist) = part1() {
+        println!("{}", dist);
+    }
+
+    if let Ok(dist) = part2() {
+        println!("{}", dist);
+    }
+}
+
+fn part1() -> io::Result<i32> {
+    let lines = read_lines()?;
 
     let mut vec1 = Vec::new();
     let mut vec2 = Vec::new();
@@ -17,12 +27,44 @@ fn main() -> io::Result<()> {
     vec1.sort();
     vec2.sort();
 
-    let dist = vec1
+    Ok(vec1
         .iter()
         .zip(vec2.iter())
         .map(|(x, y)| (x - y).abs())
-        .sum::<i32>();
+        .sum::<i32>())
+}
 
-    println!("{}", dist);
-    Ok(())
+fn part2() -> io::Result<i32> {
+    let lines = read_lines()?;
+
+    let mut vec = Vec::new();
+    let mut hash_map = HashMap::new();
+
+    for line in lines.map_while(Result::ok) {
+        let line: Vec<&str> = line.split_whitespace().collect();
+        vec.push(line[0].parse::<i32>().unwrap());
+
+        let key = line[1].parse::<i32>().unwrap();
+        if let Some(count) = hash_map.get_mut(&key) {
+            *count += 1;
+        } else {
+            hash_map.insert(key, 1);
+        }
+    }
+
+    Ok(vec
+        .into_iter()
+        .map(|x| {
+            if let Some(count) = hash_map.get(&x) {
+                *count * x
+            } else {
+                0
+            }
+        })
+        .sum::<i32>())
+}
+
+fn read_lines() -> io::Result<io::Lines<io::BufReader<File>>> {
+    let file = File::open("./input.txt")?;
+    Ok(io::BufReader::new(file).lines())
 }
