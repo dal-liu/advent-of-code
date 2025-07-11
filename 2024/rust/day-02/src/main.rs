a827d98edff4e9fd56aa6817bab56d02028e8f0a
Daniel <daniel.albert.liu@gmail.com>
2025-07-10 22:22:28 -0400

    Day 2 part 2

--- 2024/rust/day-02/src/main.rs
+++ 2024/rust/day-02/src/main.rs
@@ -5,12 +5,15 @@ fn main() {
     if let Ok(count) = part1() {
         println!("{}", count);
     }
+    if let Ok(count) = part2() {
+        println!("{}", count);
+    }
 }
 
 fn part1() -> io::Result<i32> {
     let lines = read_lines()?;
 
-    Ok(lines
+    let count = lines
         .map_while(Result::ok)
         .filter(|line| {
             line.split_whitespace()
@@ -18,7 +21,30 @@ fn part1() -> io::Result<i32> {
                 .collect::<Result<Vec<_>, _>>()
                 .map_or(false, |row| is_safe(&row))
         })
-        .count() as i32)
+        .count();
+
+    Ok(count as i32)
+}
+
+fn part2() -> io::Result<i32> {
+    let lines = read_lines()?;
+
+    let count = lines
+        .map_while(Result::ok)
+        .filter(|line| {
+            line.split_whitespace()
+                .map(str::parse::<i32>)
+                .collect::<Result<Vec<_>, _>>()
+                .map_or(false, |row| {
+                    (0..row.len()).any(|i| {
+                        let new_row = remove_element(&row, i);
+                        is_safe(&new_row)
+                    })
+                })
+        })
+        .count();
+
+    Ok(count as i32)
 }
 
 fn is_safe(line: &[i32]) -> bool {
@@ -35,6 +61,14 @@ fn is_safe(line: &[i32]) -> bool {
         }
         None => true,
     }
+}
+
+fn remove_element(line: &[i32], index: usize) -> Vec<i32> {
+    let mut new_line = line.to_vec();
+    if index < new_line.len() {
+        new_line.remove(index);
+    }
+    new_line
 }
 
 fn read_lines() -> io::Result<io::Lines<io::BufReader<File>>> {
